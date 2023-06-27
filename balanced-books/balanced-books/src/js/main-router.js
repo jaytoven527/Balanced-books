115698508aad45433f466fb9a3503ce8a9f24670
jaytoven527 <jjnorwood527@gmail.com>
2023-06-27 11:49:23 -0500

    initial commit

---
+++ balanced-books/balanced-books/src/js/main-router.js
@@ -0,0 +1,30 @@
+import '../scss/style.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  manageLocation();
+}
+
+const routes = {
+  404: "/src/404.html", 
+  "/": "/src/home.html", 
+  "/about": "/src/about.html",
+}
+
+const manageLocation = async () => {
+  const path = window.location.pathname;
+  const route = routes[path] || routes[404];
+  const html = await fetch(route).then(data => data.text());
+  document.querySelector("#app").innerHTML = html;
+}
+
+
+window.onpopstate = manageLocation();
+window.route = route;
+
+manageLocation();
